6999ba53e4db416ebe2f8f3de36b5f3352492584
naapeli <aatu.selkee@gmail.com>
2024-02-13 12:53:25 +0200

    Modified doubled pawn mask to work correctly based on the index of the square

--- Javascript projects/Chess engine/engine/bitboardMasks.js
+++ Javascript projects/Chess engine/engine/bitboardMasks.js
@@ -4,17 +4,23 @@ const cutMask = 0xFFFFFFFFFFFFFFFFn;
 const whitePassedPawnMask = new Array(64);
 const blackPassedPawnMask = new Array(64);
 
+const doubledPawnMask = new Array(64);
 const isolatedPawnMask = new Array(8);
-const doubledPawnMask = new Array(8);
 
 function initBitboards() {
     for (let index = 0; index < 64; index++) {
         const [i, j] = [index % 8, Math.floor(index / 8)];
         const fileMask = (hFileMask << BigInt(i)) & cutMask;
-        doubledPawnMask[j] = fileMask;
+        doubledPawnMask[index] = fileMask & (cutMask ^ (BigInt(1) << BigInt(index)));
         const rightMask = (hFileMask << BigInt(Math.min(i + 1, 7))) & cutMask;
         const leftmask = (hFileMask << BigInt(Math.max(i - 1, 0))) & cutMask;
-        isolatedPawnMask[j] = rightMask & leftmask;
+        if (i == 0) {
+            isolatedPawnMask[i] = rightMask;
+        } else if (i == 7) {
+            isolatedPawnMask[i] = leftmask;
+        } else {
+            isolatedPawnMask[i] = rightMask | leftmask;
+        };
         const tripleFileMask = fileMask | rightMask | leftmask;
         const whiteFrontMask = (0xFFFFFFFFFFFFFFFFn >> BigInt(8 * (8 - j))) & cutMask;
         const blackFrontMask = (0xFFFFFFFFFFFFFFFFn << BigInt(8 * (j + 1))) & cutMask;
