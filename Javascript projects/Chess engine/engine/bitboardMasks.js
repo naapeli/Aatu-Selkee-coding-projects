3d747fdc9440821c26210953fada287cf0aa99b2
naapeli <aatu.selkee@gmail.com>
2024-02-13 20:45:23 +0200

    Added masks for king pawn shield

--- Javascript projects/Chess engine/engine/bitboardMasks.js
+++ Javascript projects/Chess engine/engine/bitboardMasks.js
@@ -1,11 +1,15 @@
 const hFileMask = 0x0101010101010101n;
 const cutMask = 0xFFFFFFFFFFFFFFFFn;
+const singleRankMask = 0xFFn;
 
 const whitePassedPawnMask = new Array(64);
 const blackPassedPawnMask = new Array(64);
 
 const doubledPawnMask = new Array(64);
 const isolatedPawnMask = new Array(8);
+
+const whiteKingPawnShield = new Array(64);
+const blackKingPawnShield = new Array(64);
 
 function initBitboards() {
     for (let index = 0; index < 64; index++) {
@@ -22,10 +26,15 @@ function initBitboards() {
             isolatedPawnMask[i] = rightMask | leftmask;
         };
         const tripleFileMask = fileMask | rightMask | leftmask;
-        const whiteFrontMask = (0xFFFFFFFFFFFFFFFFn >> BigInt(8 * (8 - j))) & cutMask;
-        const blackFrontMask = (0xFFFFFFFFFFFFFFFFn << BigInt(8 * (j + 1))) & cutMask;
+        const whiteFrontMask = (cutMask >> BigInt(8 * (8 - j))) & cutMask;
+        const blackFrontMask = (cutMask << BigInt(8 * (j + 1))) & cutMask;
         whitePassedPawnMask[index] = whiteFrontMask & tripleFileMask;
         blackPassedPawnMask[index] = blackFrontMask & tripleFileMask;
+
+        const whiteKingFrontMask = ((singleRankMask << BigInt(8 * (j - 1))) | (singleRankMask << BigInt(8 * (j - 2)))) & cutMask;
+        const blackKingFrontMask = ((singleRankMask << BigInt(8 * (j + 1))) | (singleRankMask << BigInt(8 * (j + 2)))) & cutMask;
+        whiteKingPawnShield[index] = whiteKingFrontMask & tripleFileMask;
+        blackKingPawnShield[index] = blackKingFrontMask & tripleFileMask;
     };
 };
 
