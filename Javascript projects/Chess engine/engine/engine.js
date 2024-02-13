2cc1afee86cd2dbc30ef57bf528d84939d425cfe
naapeli <aatu.selkee@gmail.com>
2024-02-13 11:09:03 +0200

    Modified getpassedpawnbonus to just look up the needed mask

--- Javascript projects/Chess engine/engine/engine.js
+++ Javascript projects/Chess engine/engine/engine.js
@@ -518,20 +518,13 @@ class engine {
     };
 
     getPassedPawnBonus(color) {
-        const hFileMask = 0x0101010101010101n;
-        const cutMask = 0xFFFFFFFFFFFFFFFFn;
         const pawnSquares = this.board.pieces[color + "P"];
         const enemyColor = color == "w" ? "b" : "w";
         const enemyPawnMask = this.board.pieceBitBoards[enemyColor + "P"];
         let bonus = 0;
         for (let index of pawnSquares) {
             const [i, j] = this.board.boardUtility.indexToSquare(index);
-            const fileMask = (hFileMask << BigInt(i)) & cutMask;
-            const rightMask = (hFileMask << BigInt(Math.min(i + 1, 7))) & cutMask;
-            const leftmask = (hFileMask << BigInt(Math.max(i - 1, 0))) & cutMask;
-            const tripleFileMask = fileMask | rightMask | leftmask;
-            const frontMask = color == "w" ? (0xFFFFFFFFFFFFFFFFn >> BigInt(8 * (8 - j))) & cutMask : (0xFFFFFFFFFFFFFFFFn << BigInt(8 * (j + 1))) & cutMask;
-            const passedPawnMask = tripleFileMask & frontMask;
+            const passedPawnMask = color == "w" ? whitePassedPawnMask[index] : blackPassedPawnMask[index];
             const isPassedPawn = (enemyPawnMask & passedPawnMask) == BigInt(0);
             if (isPassedPawn) {
                 bonus += color == "w" ? (7 - j) * 20 : 20 * j;
